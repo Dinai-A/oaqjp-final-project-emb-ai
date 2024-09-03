7f5ff2a0cb54aa2a93abcb9485e81746a9a29398
Dino <dino.tatsu@gmail.com>
2024-09-03 19:21:15 +0600

    task 6: emotion detection web application

--- server.py
+++ server.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
 
+@app.route("/")
+def index_view():
+    return render_template('index.html')
+
+
 @app.route("/emotionDetector")
-def emotionDetector():
-    return "some text"
+def emotion_detector_view():
+    text_to_analyze = request.args["textToAnalyze"]
+    data = emotion_detector(text_to_analyze)
+    response_str = (
+        f"For the given statement, the system response is 'anger': {data['anger']}, 'disgust': {data['disgust']},"
+        f" 'fear': {data['fear']}, 'joy': {data['joy']} and 'sadness': {data['sadness']}. "
+        f"The dominant emotion is {data['dominant_emotion']}."
+    )
+    return response_str
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
